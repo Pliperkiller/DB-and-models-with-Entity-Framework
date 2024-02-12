bd328c97e3bb5a189b6f867a1afa8213616fc1d3
pliperkiller <pip.kro@gmail.com>
2024-02-12 00:11:33 -0500

    Fluent API integration for creating a DB in localhost

---
+++ Models/Categoria.cs
@@ -0,0 +1,14 @@
+//using System.ComponentModel.DataAnnotations;
+public class Categoria
+{
+    //Data annotation if using without fluent API
+    //[Key]
+    public Guid CategoriaId {get;set;}
+
+    //[Required]
+    //[MaxLength(150)]
+    public string Nombre {get;set;}
+    public string Descripcion {get;set;}
+
+    public ICollection<Tarea> Tareas {get;set;}
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//DB in memory
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
+//SQL Server
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("cnTareas"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/dbconexion",async ([FromServices] TareasContext dbContext)=>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
